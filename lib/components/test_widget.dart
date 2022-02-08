1414901ba4597c6beb79ff40b26526653223aaa1
Prasad Sugirda <prasthsugirtha@gmail.com>
2022-02-08 13:02:00 +0530

    feat: components added

---
+++ lib/components/test_widget.dart
@@ -0,0 +1,19 @@
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TestWidget extends StatefulWidget {
+  const TestWidget({Key key}) : super(key: key);
+
+  @override
+  _TestWidgetState createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
