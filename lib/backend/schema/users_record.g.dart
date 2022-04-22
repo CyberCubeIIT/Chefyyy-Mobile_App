9dd9a0fc20f2bf6a3e8e5e98192dd694830b0e27
AnupammaNarampanawa <anupama.20200736@iit.ac.lk>
2022-04-22 21:25:04 +0530

    user record dart modified

---
+++ lib/backend/schema/users_record.g.dart
@@ -0,0 +1,523 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'users_record.dart';
+
+// **************************************************************************
+// BuiltValueGenerator
+// **************************************************************************
+
+Serializer<UsersRecord> _$usersRecordSerializer = new _$UsersRecordSerializer();
+
+class _$UsersRecordSerializer implements StructuredSerializer<UsersRecord> {
+  @override
+  final Iterable<Type> types = const [UsersRecord, _$UsersRecord];
+  @override
+  final String wireName = 'UsersRecord';
+
+  @override
+  Iterable<Object> serialize(Serializers serializers, UsersRecord object,
+      {FullType specifiedType = FullType.unspecified}) {
+    final result = <Object>[];
+    Object value;
+    value = object.firstName;
+    if (value != null) {
+      result
+        ..add('first_name')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(String)));
+    }
+    value = object.lastName;
+    if (value != null) {
+      result
+        ..add('last_name')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(String)));
+    }
+    value = object.age;
+    if (value != null) {
+      result
+        ..add('age')
+        ..add(serializers.serialize(value, specifiedType: const FullType(int)));
+    }
+    value = object.height;
+    if (value != null) {
+      result
+        ..add('height')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(double)));
+    }
+    value = object.weight;
+    if (value != null) {
+      result
+        ..add('weight')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(double)));
+    }
+    value = object.gender;
+    if (value != null) {
+      result
+        ..add('gender')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(String)));
+    }
+    value = object.allergies;
+    if (value != null) {
+      result
+        ..add('allergies')
+        ..add(serializers.serialize(value,
+            specifiedType:
+                const FullType(BuiltList, const [const FullType(String)])));
+    }
+    value = object.diseases;
+    if (value != null) {
+      result
+        ..add('diseases')
+        ..add(serializers.serialize(value,
+            specifiedType:
+                const FullType(BuiltList, const [const FullType(String)])));
+    }
+    value = object.email;
+    if (value != null) {
+      result
+        ..add('email')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(String)));
+    }
+    value = object.displayName;
+    if (value != null) {
+      result
+        ..add('display_name')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(String)));
+    }
+    value = object.workout;
+    if (value != null) {
+      result
+        ..add('workout')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(String)));
+    }
+    value = object.photoUrl;
+    if (value != null) {
+      result
+        ..add('photo_url')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(String)));
+    }
+    value = object.uid;
+    if (value != null) {
+      result
+        ..add('uid')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(String)));
+    }
+    value = object.createdTime;
+    if (value != null) {
+      result
+        ..add('created_time')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(DateTime)));
+    }
+    value = object.phoneNumber;
+    if (value != null) {
+      result
+        ..add('phone_number')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(String)));
+    }
+    value = object.reference;
+    if (value != null) {
+      result
+        ..add('Document__Reference__Field')
+        ..add(serializers.serialize(value,
+            specifiedType: const FullType(
+                DocumentReference, const [const FullType(Object)])));
+    }
+    return result;
+  }
+
+  @override
+  UsersRecord deserialize(Serializers serializers, Iterable<Object> serialized,
+      {FullType specifiedType = FullType.unspecified}) {
+    final result = new UsersRecordBuilder();
+
+    final iterator = serialized.iterator;
+    while (iterator.moveNext()) {
+      final key = iterator.current as String;
+      iterator.moveNext();
+      final Object value = iterator.current;
+      switch (key) {
+        case 'first_name':
+          result.firstName = serializers.deserialize(value,
+              specifiedType: const FullType(String)) as String;
+          break;
+        case 'last_name':
+          result.lastName = serializers.deserialize(value,
+              specifiedType: const FullType(String)) as String;
+          break;
+        case 'age':
+          result.age = serializers.deserialize(value,
+              specifiedType: const FullType(int)) as int;
+          break;
+        case 'height':
+          result.height = serializers.deserialize(value,
+              specifiedType: const FullType(double)) as double;
+          break;
+        case 'weight':
+          result.weight = serializers.deserialize(value,
+              specifiedType: const FullType(double)) as double;
+          break;
+        case 'gender':
+          result.gender = serializers.deserialize(value,
+              specifiedType: const FullType(String)) as String;
+          break;
+        case 'allergies':
+          result.allergies.replace(serializers.deserialize(value,
+                  specifiedType:
+                      const FullType(BuiltList, const [const FullType(String)]))
+              as BuiltList<Object>);
+          break;
+        case 'diseases':
+          result.diseases.replace(serializers.deserialize(value,
+                  specifiedType:
+                      const FullType(BuiltList, const [const FullType(String)]))
+              as BuiltList<Object>);
+          break;
+        case 'email':
+          result.email = serializers.deserialize(value,
+              specifiedType: const FullType(String)) as String;
+          break;
+        case 'display_name':
+          result.displayName = serializers.deserialize(value,
+              specifiedType: const FullType(String)) as String;
+          break;
+        case 'workout':
+          result.workout = serializers.deserialize(value,
+              specifiedType: const FullType(String)) as String;
+          break;
+        case 'photo_url':
+          result.photoUrl = serializers.deserialize(value,
+              specifiedType: const FullType(String)) as String;
+          break;
+        case 'uid':
+          result.uid = serializers.deserialize(value,
+              specifiedType: const FullType(String)) as String;
+          break;
+        case 'created_time':
+          result.createdTime = serializers.deserialize(value,
+              specifiedType: const FullType(DateTime)) as DateTime;
+          break;
+        case 'phone_number':
+          result.phoneNumber = serializers.deserialize(value,
+              specifiedType: const FullType(String)) as String;
+          break;
+        case 'Document__Reference__Field':
+          result.reference = serializers.deserialize(value,
+                  specifiedType: const FullType(
+                      DocumentReference, const [const FullType(Object)]))
+              as DocumentReference<Object>;
+          break;
+      }
+    }
+
+    return result.build();
+  }
+}
+
+class _$UsersRecord extends UsersRecord {
+  @override
+  final String firstName;
+  @override
+  final String lastName;
+  @override
+  final int age;
+  @override
+  final double height;
+  @override
+  final double weight;
+  @override
+  final String gender;
+  @override
+  final BuiltList<String> allergies;
+  @override
+  final BuiltList<String> diseases;
+  @override
+  final String email;
+  @override
+  final String displayName;
+  @override
+  final String workout;
+  @override
+  final String photoUrl;
+  @override
+  final String uid;
+  @override
+  final DateTime createdTime;
+  @override
+  final String phoneNumber;
+  @override
+  final DocumentReference<Object> reference;
+
+  factory _$UsersRecord([void Function(UsersRecordBuilder) updates]) =>
+      (new UsersRecordBuilder()..update(updates)).build();
+
+  _$UsersRecord._(
+      {this.firstName,
+      this.lastName,
+      this.age,
+      this.height,
+      this.weight,
+      this.gender,
+      this.allergies,
+      this.diseases,
+      this.email,
+      this.displayName,
+      this.workout,
+      this.photoUrl,
+      this.uid,
+      this.createdTime,
+      this.phoneNumber,
+      this.reference})
+      : super._();
+
+  @override
+  UsersRecord rebuild(void Function(UsersRecordBuilder) updates) =>
+      (toBuilder()..update(updates)).build();
+
+  @override
+  UsersRecordBuilder toBuilder() => new UsersRecordBuilder()..replace(this);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is UsersRecord &&
+        firstName == other.firstName &&
+        lastName == other.lastName &&
+        age == other.age &&
+        height == other.height &&
+        weight == other.weight &&
+        gender == other.gender &&
+        allergies == other.allergies &&
+        diseases == other.diseases &&
+        email == other.email &&
+        displayName == other.displayName &&
+        workout == other.workout &&
+        photoUrl == other.photoUrl &&
+        uid == other.uid &&
+        createdTime == other.createdTime &&
+        phoneNumber == other.phoneNumber &&
+        reference == other.reference;
+  }
+
+  @override
+  int get hashCode {
+    return $jf($jc(
+        $jc(
+            $jc(
+                $jc(
+                    $jc(
+                        $jc(
+                            $jc(
+                                $jc(
+                                    $jc(
+                                        $jc(
+                                            $jc(
+                                                $jc(
+                                                    $jc(
+                                                        $jc(
+                                                            $jc(
+                                                                $jc(
+                                                                    0,
+                                                                    firstName
+                                                                        .hashCode),
+                                                                lastName
+                                                                    .hashCode),
+                                                            age.hashCode),
+                                                        height.hashCode),
+                                                    weight.hashCode),
+                                                gender.hashCode),
+                                            allergies.hashCode),
+                                        diseases.hashCode),
+                                    email.hashCode),
+                                displayName.hashCode),
+                            workout.hashCode),
+                        photoUrl.hashCode),
+                    uid.hashCode),
+                createdTime.hashCode),
+            phoneNumber.hashCode),
+        reference.hashCode));
+  }
+
+  @override
+  String toString() {
+    return (newBuiltValueToStringHelper('UsersRecord')
+          ..add('firstName', firstName)
+          ..add('lastName', lastName)
+          ..add('age', age)
+          ..add('height', height)
+          ..add('weight', weight)
+          ..add('gender', gender)
+          ..add('allergies', allergies)
+          ..add('diseases', diseases)
+          ..add('email', email)
+          ..add('displayName', displayName)
+          ..add('workout', workout)
+          ..add('photoUrl', photoUrl)
+          ..add('uid', uid)
+          ..add('createdTime', createdTime)
+          ..add('phoneNumber', phoneNumber)
+          ..add('reference', reference))
+        .toString();
+  }
+}
+
+class UsersRecordBuilder implements Builder<UsersRecord, UsersRecordBuilder> {
+  _$UsersRecord _$v;
+
+  String _firstName;
+  String get firstName => _$this._firstName;
+  set firstName(String firstName) => _$this._firstName = firstName;
+
+  String _lastName;
+  String get lastName => _$this._lastName;
+  set lastName(String lastName) => _$this._lastName = lastName;
+
+  int _age;
+  int get age => _$this._age;
+  set age(int age) => _$this._age = age;
+
+  double _height;
+  double get height => _$this._height;
+  set height(double height) => _$this._height = height;
+
+  double _weight;
+  double get weight => _$this._weight;
+  set weight(double weight) => _$this._weight = weight;
+
+  String _gender;
+  String get gender => _$this._gender;
+  set gender(String gender) => _$this._gender = gender;
+
+  ListBuilder<String> _allergies;
+  ListBuilder<String> get allergies =>
+      _$this._allergies ??= new ListBuilder<String>();
+  set allergies(ListBuilder<String> allergies) => _$this._allergies = allergies;
+
+  ListBuilder<String> _diseases;
+  ListBuilder<String> get diseases =>
+      _$this._diseases ??= new ListBuilder<String>();
+  set diseases(ListBuilder<String> diseases) => _$this._diseases = diseases;
+
+  String _email;
+  String get email => _$this._email;
+  set email(String email) => _$this._email = email;
+
+  String _displayName;
+  String get displayName => _$this._displayName;
+  set displayName(String displayName) => _$this._displayName = displayName;
+
+  String _workout;
+  String get workout => _$this._workout;
+  set workout(String workout) => _$this._workout = workout;
+
+  String _photoUrl;
+  String get photoUrl => _$this._photoUrl;
+  set photoUrl(String photoUrl) => _$this._photoUrl = photoUrl;
+
+  String _uid;
+  String get uid => _$this._uid;
+  set uid(String uid) => _$this._uid = uid;
+
+  DateTime _createdTime;
+  DateTime get createdTime => _$this._createdTime;
+  set createdTime(DateTime createdTime) => _$this._createdTime = createdTime;
+
+  String _phoneNumber;
+  String get phoneNumber => _$this._phoneNumber;
+  set phoneNumber(String phoneNumber) => _$this._phoneNumber = phoneNumber;
+
+  DocumentReference<Object> _reference;
+  DocumentReference<Object> get reference => _$this._reference;
+  set reference(DocumentReference<Object> reference) =>
+      _$this._reference = reference;
+
+  UsersRecordBuilder() {
+    UsersRecord._initializeBuilder(this);
+  }
+
+  UsersRecordBuilder get _$this {
+    final $v = _$v;
+    if ($v != null) {
+      _firstName = $v.firstName;
+      _lastName = $v.lastName;
+      _age = $v.age;
+      _height = $v.height;
+      _weight = $v.weight;
+      _gender = $v.gender;
+      _allergies = $v.allergies?.toBuilder();
+      _diseases = $v.diseases?.toBuilder();
+      _email = $v.email;
+      _displayName = $v.displayName;
+      _workout = $v.workout;
+      _photoUrl = $v.photoUrl;
+      _uid = $v.uid;
+      _createdTime = $v.createdTime;
+      _phoneNumber = $v.phoneNumber;
+      _reference = $v.reference;
+      _$v = null;
+    }
+    return this;
+  }
+
+  @override
+  void replace(UsersRecord other) {
+    ArgumentError.checkNotNull(other, 'other');
+    _$v = other as _$UsersRecord;
+  }
+
+  @override
+  void update(void Function(UsersRecordBuilder) updates) {
+    if (updates != null) updates(this);
+  }
+
+  @override
+  _$UsersRecord build() {
+    _$UsersRecord _$result;
+    try {
+      _$result = _$v ??
+          new _$UsersRecord._(
+              firstName: firstName,
+              lastName: lastName,
+              age: age,
+              height: height,
+              weight: weight,
+              gender: gender,
+              allergies: _allergies?.build(),
+              diseases: _diseases?.build(),
+              email: email,
+              displayName: displayName,
+              workout: workout,
+              photoUrl: photoUrl,
+              uid: uid,
+              createdTime: createdTime,
+              phoneNumber: phoneNumber,
+              reference: reference);
+    } catch (_) {
+      String _$failedField;
+      try {
+        _$failedField = 'allergies';
+        _allergies?.build();
+        _$failedField = 'diseases';
+        _diseases?.build();
+      } catch (e) {
+        throw new BuiltValueNestedFieldError(
+            'UsersRecord', _$failedField, e.toString());
+      }
+      rethrow;
+    }
+    replace(_$result);
+    return _$result;
+  }
+}
+
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
